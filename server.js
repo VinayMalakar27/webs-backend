715d0aff02e5892ecb202497505e1b149ab31b16
VinayMalakar27 <vinaymalakar576@gmail,com>
2025-11-12 03:57:01 +0530

    tw

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {origin: "*",
+  credentials: true}
+));
 app.use(express.json());
 
 // Ensure uploads folder exists
